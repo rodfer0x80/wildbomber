93054aecd78a07f96bb955e85ad96698ac48cc37
rodfer0x80 <rodfer0x80@proton.me>
2023-02-09 17:37:56 +0000

    [*] now in anarchy teddy sleeps

---
+++ scan_common_endpoints.py
@@ -0,0 +1,4 @@
+# scan webserver for common endpoints
+# use compiled list from crappy lists we already have plus internet research
+# including both subdomains and directories
+# like wild_dirs.txt and wild_subs.txt
---
+++ scan_js_files.py
@@ -0,0 +1,6 @@
+# scan all js files
+# find api endpoints
+# list them for further querying
+# since we are at it also scan for keys
+# will need another list for this keys aws_key/s key youtube_key/s etc
+# https://www.bugbountyhunter.com/guides/?type=javascript_files
